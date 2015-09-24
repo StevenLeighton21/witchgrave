e5d26b877277e30c05c6fe96bb4ce9d10b2ccd17
Steven <steven.leighton21@gmail.com>
2015-09-24 15:46:32 +0100

    wip

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,9 @@
 class Campaign < ActiveRecord::Base
-	validates :description, length: { maximum: 500}
+	belongs_to :user
+	has_many :user
+	validates :description, length: { maximum: 500},
+							presence: true
+	validates :name, length: {maximum: 128},
+					  presence: true
+	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
 	has_many :campaign
 	belongs_to :campaign
+
+	validates :name, length: {maximum: 128},
+				 	 presence: true
+	validates :email, presence: true
 end
