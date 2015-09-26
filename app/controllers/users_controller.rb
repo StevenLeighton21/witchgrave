755836f2cf987b08e693611b1c024e5c774f6c61
Steven Leighton <steven.leighton21@gmail.com>
2015-09-26 16:36:06 +0100

    fix user errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,9 +25,11 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
 
+  def update
+    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      # Handle a successful update.
       flash[:success] = "Profile updated"
       redirect_to @user
     else
@@ -47,20 +49,6 @@ class UsersController < ApplicationController
         
     else
       render'new'
-    end
-  end
-
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
     end
   end
 
