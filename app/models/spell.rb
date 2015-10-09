bce5d1a1e13b5077b0faa220d33f4a1f41eb2a17
Steven Leighton <steven.leighton21@gmail.com>
2015-10-09 19:09:23 +0100

    minor quality updates

--- app/models/spell.rb
+++ app/models/spell.rb
@@ -12,12 +12,67 @@ class Spell < ActiveRecord::Base
     spell_discipline  = self.discipline
     cast_value        = self.casting_value.to_i
     aligned_value     = cast_value.to_i + 2 
+    opposed_value     = cast_value.to_i + 6
+    neutral_value     = cast_value.to_i + 4 
 
-    if wizard_discipline.eql?(spell_discipline)
+    if wizard_discipline.eql?(spell_discipline) #same
       return "<span style='color:green'> #{cast_value} </span>".html_safe
-    #elsif # aligned todo
-    else # opposed
-      return "<span style='color:red'> #{aligned_value} </span>".html_safe
+    elsif aligned_for(wizard_discipline).include?(spell_discipline)  #aligned
+      return "<span style='color:blue'> #{aligned_value} </span>".html_safe
+    elsif spell_discipline.eql?(opposed_for(wizard_discipline)) # opposed
+      return "<span style='color:red'> #{opposed_value} </span>".html_safe
+    else #must be neutral
+      return "<span style='color:orange'> #{neutral_value} </span>".html_safe
+    end
+  end
+
+  def aligned_for(discipline)
+    case discipline
+    when "Chronomancer"
+      return ["Necromancer", "Soothsayer", "Elementalist"]
+    when "Elementalist"
+      return ["Chronomancer", "Enchanter", "Chronomancer"]
+    when "Enchanter"
+      return ["Witch", "Sigilist", "Elementalist"]
+    when "Illusionist"
+      return ["Sigilist", "Soothsayer", "Thaumaturge"]
+    when "Necromancer"
+      return ["Witch", "Chronomancer", "Summoner"]
+    when "Sigilist"
+      return ["Thaumaturge", "Illusionist", "Enchanter"]
+    when "Soothsayer"
+      return ["Thaumaturge", "Chronomancer", "Illusionist"]
+    when "Summoner"
+      return ["Necromancer", "Witch", "Elementalist"]
+    when "Thaumaturge"
+      return ["Illusionist", "Soothsayer", "Sigilist"]
+    when "Witch"
+      return ["Enchanter", "Necromancer", "Summoner"]
+    end
+  end
+
+  def opposed_for(discipline)
+    case discipline
+    when "Chronomancer"
+      return "Enchanter"
+    when "Elementalist"
+      return "Illusionist"
+    when "Enchanter"
+      return "Chronomancer"
+    when "Illusionist"
+      return "Elementalist"
+    when "Necromancer"
+      return "Thaumaturge"
+    when "Sigilist"
+      return "Summoner"
+    when "Soothsayer"
+      return "Witch"
+    when "Summoner"
+      return "Sigilist"
+    when "Thaumaturge"
+      return "Necromancer"
+    when "Witch"
+      return "Soothsayer"
     end
   end
 end
