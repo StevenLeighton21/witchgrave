ac722b4b28ce14653e4177911f1e1fed81b2d8ad
Steven Leighton <steven.leighton21@gmail.com>
2015-10-11 08:19:19 +0100

    Fixed some visual bugs

--- app/models/frost_grave_participant.rb
+++ app/models/frost_grave_participant.rb
@@ -17,4 +17,42 @@ class FrostGraveParticipant < ActiveRecord::Base
   		errors.add(:campaign_password, "Invalid Campaign Authorisation. Check with the Campaign's creator...")
   	end
   end
+
+  def string_for_wizard_inventory
+    string = "#{self.wizard_inventory_1}"
+    if self.wizard_inventory_2.nil? or self.wizard_inventory_2.eql?("")
+    else
+      string = string + ", #{wizard_inventory_2}"
+    end
+    if self.wizard_inventory_3.nil? or self.wizard_inventory_3.eql?("")
+    else
+      string = string + ", #{wizard_inventory_3}"
+    end
+    if self.wizard_inventory_4.nil? or self.wizard_inventory_4.eql?("")
+    else
+      string = string + ", #{wizard_inventory_4}"
+    end
+    if self.wizard_inventory_5.nil? or self.wizard_inventory_5.eql?("")
+    else
+      string = string + ", #{wizard_inventory_5}"
+    end
+    return string
+  end
+
+  def string_for_apprentice_inventory
+    string = "#{self.apprentice_inventory_1}"
+    if self.apprentice_inventory_2.nil? or self.apprentice_inventory_2.eql?("")
+    else
+      string = string + ", #{apprentice_inventory_2}"
+    end
+    if self.apprentice_inventory_3.nil? or self.apprentice_inventory_3.eql?("")
+    else
+      string = string + ", #{apprentice_inventory_3}"
+    end
+    if self.apprentice_inventory_4.nil? or self.apprentice_inventory_4.eql?("")
+    else
+      string = string + ", #{apprentice_inventory_4}"
+    end
+    return string
+  end
 end
